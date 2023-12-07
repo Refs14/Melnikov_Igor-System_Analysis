fd4fa6fbc08007007c3514c8b3b8f917458d99a1
Refs14 <126952245+Refs14@users.noreply.github.com>
2023-12-07 23:30:17 +0300

    Update task.py

--- task1/task.py
+++ task1/task.py
@@ -1,1 +1,10 @@
+import csv
 
+file_name = 'data.csv'
+in_ = input().split(' ')
+a = int(in_[0])
+b = int(in_[1])
+
+with open('data.csv', newline='') as csvfile:
+    data = list(csv.reader(csvfile, delimiter=',', quotechar='|'))
+    print(data[a][b])

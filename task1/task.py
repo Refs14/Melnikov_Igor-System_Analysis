a0f5d03e62c30811215ac1bf2610d2f0e595b6c8
Refs14 <126952245+Refs14@users.noreply.github.com>
2023-12-10 02:48:18 +0300

    task1

--- task1/task.py
+++ task1/task.py
@@ -5,6 +5,7 @@ in_ = input().split(' ')
 a = int(in_[0])
 b = int(in_[1])
 
+
 with open('data.csv', newline='') as csvfile:
     data = list(csv.reader(csvfile, delimiter=',', quotechar='|'))
     print(data[a][b])

c6ea703e5ebb45ccf87479190ab95e8fd7525dde
Refs14 <126952245+Refs14@users.noreply.github.com>
2024-01-12 12:12:50 +0300

    Update task.py

--- task6/task.py
+++ task6/task.py
@@ -4,24 +4,33 @@ def task(range1, range2, range3):
     r1 = json.loads(range1)
     r2 = json.loads(range2)
     r3 = json.loads(range3)
+    rank1 = [int(el[1]) for el in r1]
+    rank2 = [int(el[1]) for el in r2]
+    rank3 = [int(el[1]) for el in r3]
+    n = len(rank1)
+    concordant = 0
+    discordant = 0
+    for i in range(n):
+        for j in range(i+1, n):
+            # Проверяем каждую пару элементов из ранжировок
+            for rank in [rank1, rank2, rank3]:
+                if (rank[i] < rank[j] and rank2[i] < rank2[j] and rank3[i] < rank3[j]) or (rank[i] > rank[j] and rank2[i] > rank2[j] and rank3[i] > rank3[j]):
+                    # Если все три ранжировки согласованы (все три пары имеют одинаковый порядок)
+                    concordant += 1
+                else:
+                    # В противном случае - несогласованные пары
+                    discordant += 1
+    # Коэффициент Кендалла
+    kendall_tau = (concordant - discordant) / (concordant + discordant)
+    return kendall_tau
 
-    size = len(r1)
-    sums = []
-    for i in range(size):
-        isum = 0
-        isum += r1.index(f'O{i+1}')
-        isum += r2.index(f'O{i+1}')
-        isum += r3.index(f'O{i+1}')
-        sums.append(isum)
-    Dmax_sums = []
-    for i in range(size):
-        Dmax_sums.append(3*(i+1))
-    average = sum(sums)/size
-    D = sum([(i - average)**2 for i in sums])/(size-1)
-    Dmax = sum([(i - average)**2 for i in Dmax_sums])/(size-1)
-    return round(D/Dmax, 2)
-    
+# Пример использования
 range1 = '["O1","O2","O3"]'
 range2 = '["O1","O3","O2"]'
 range3 = '["O1","O3","O2"]'
-print(task(range1, range2, range3))
+print("Коэффициент Кендалла:", task(range1, range2, range3))
+
+range1 = '["O1","O2","O3"]'
+range2 = '["O1","O2","O3"]'
+range3 = '["O1","O2","O3"]'
+print("Коэффициент Кендалла:", task(range1, range2, range3))
